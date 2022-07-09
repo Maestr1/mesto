924aa25a030346a1cc3a247997ebe0eb8bbf0269
Maestr1 <maestr234@gmail.com>
2022-07-09 23:50:03 +0300

    fix: исправить размер картинок, рефактор скрипта

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,43 @@
 let popup = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let formName = document.querySelector('.popup__name');
-let formJob = document.querySelector('.popup__job');
+let formName = document.querySelector('.popup__input_type_name');
+let formJob = document.querySelector('.popup__input_type_job');
 let editBtn = document.querySelector('.profile__edit-btn');
 let closeBtn = document.querySelector('.popup__close-btn');
 
-// Функция управляет открытием и закрытием попапа
-function popupSwitch() {
-
+// Функция управляет открытием попапа
+function popupOpen() {
   editBtn.addEventListener('click', function (event) {
     popup.classList.add('popup_opened'); //добавляем класс открытия
-    formName.value = profileName.textContent;//присваиваем значения полям в попапе
-    formJob.value = profileJob.textContent;//каждый раз при открытии
-  });
-
-  closeBtn.addEventListener('click', function (event) {
-    popup.classList.remove('popup_opened');//убираем класс открытия
+    formName.value = profileName.textContent; //присваиваем значения полям в попапе
+    formJob.value = profileJob.textContent; //каждый раз при открытии
   });
 }
-popupSwitch();
 
-let formElement = document.querySelector('.popup__form')
+// Функция управляет закрытием попапа
+function popupClose() {
+  closeBtn.addEventListener('click', function (event) {
+    popup.classList.remove('popup_opened'); //убираем класс открытия
+  });
+}
+
+popupOpen()
+popupClose()
+
+let formElement = document.querySelector('.popup__form');
 
 // Функция управляет сохранением данных в строках профиля
-function formSubmitHandler (evt) {
-	evt.preventDefault();//отменяем действие по умолчанию (перезагрузка страницы при отправке)
-  
-	let nameValue = formName.value//берем значение полей формы
-	let jobValue = formJob.value
+function formSubmitHandler(evt) {
+  evt.preventDefault(); //отменяем действие по умолчанию (перезагрузка страницы при отправке)
 
-  profileName.textContent = nameValue//присваеваем элементам в профиле
-  profileJob.textContent = jobValue
+  let nameValue = formName.value; //берем значение полей формы
+  let jobValue = formJob.value;
 
-  popup.classList.remove('popup_opened');//закрываем попап
+  profileName.textContent = nameValue; //присваеваем элементам в профиле
+  profileJob.textContent = jobValue;
+
+  popup.classList.remove('popup_opened'); //закрываем попап
 }
 
-formElement.addEventListener('submit', formSubmitHandler);//функция срабатывает по событию "submit"
+formElement.addEventListener('submit', formSubmitHandler); //функция срабатывает по событию "submit"
