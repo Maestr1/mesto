238d278b1508cfe885b0a2fc1e39951f40cb70d8
Maestr1 <maestr234@gmail.com>
2022-10-04 10:32:39 +0300

    fix: исправляет класс кнопки для валидации

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,7 +3,7 @@ export const settings = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__submit-btn',
-  inactiveButtonClass: 'popup__save-btn_disabled',
+  inactiveButtonClass: 'popup__submit-btn_disabled',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__input-error_visible'
 };
